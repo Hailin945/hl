ac888184163c693cbe84cd982f8d2fb349509afd
Hailin <hailin_qy@163.com>
2019-11-08 16:49:51 +0800

    refactor

--- util/util-excel/src/main/java/com/hl/excel/test/ImportExcelTest.java
+++ util/util-excel/src/main/java/com/hl/excel/test/ImportExcelTest.java
@@ -17,14 +17,14 @@ public class ImportExcelTest {
                 "test.xls",
                 new FileInputStream("/Users/yjj/Desktop/test.xls"),
                 ImportExcel.getMap(keyValue),
-                "com.mbc.common.utils.excel.model.PhoneModelExcelTest", 1);
+                "com.hl.common.utils.excel.model.PhoneModelExcelTest", 1);
         System.out.println(list.size());
         //导出
         ImportExcel.exportExcel(
                 "导出数据",
                 new FileOutputStream("/Users/yjj/Desktop/testOut.xls"),
                 ImportExcel.getMap(keyValue), list,
-                "com.mbc.common.utils.excel.model.PhoneModelExcelTest", null, null, null);
+                "com.hl.common.utils.excel.model.PhoneModelExcelTest", null, null, null);
     }
 
 
